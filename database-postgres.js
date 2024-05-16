82add817cc5603ee9cbdffb91db096ef40102b63
Matheus Arendartchuk <matheusarendartchuk@gmail.com>
2024-05-16 04:24:09 -0300

    v1

---
+++ database-postgres.js
@@ -0,0 +1,40 @@
+import { randomUUID } from "crypto"
+import { sql } from './db.js'
+
+export class DatabasePostgres {
+    async list(search) {
+        let videos
+
+        //comandos sql
+        if (search) {
+            videos = sql`select * from videos where title ilike ${'%' +search + '%'}`
+        } else {
+            videos = sql`select * from videos`
+        }
+
+        return videos
+    }
+
+    async create(video) {
+        const videoId = randomUUID()
+        const { title, description, duration } = video
+        
+        //comando sql
+        await sql`insert into videos (id, title, description, duration)
+        VALUES (${videoId}, ${title}, ${description}, ${duration})`
+    }
+
+    async update(id, video) {
+        const { title, description, duration} = video
+        
+        //comando sql
+        await sql`update videos set title = ${title},
+        description = ${description},
+        duration = ${duration}
+        WHERE id = ${id}`
+    }
+
+    async delete(id) {
+        await sql`delete from videos where id = ${id}`
+    }
+}
